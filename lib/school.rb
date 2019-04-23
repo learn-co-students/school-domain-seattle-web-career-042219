de1d0f644738d0a52e92102f122d746358dd3c0a
Flatiron School <jjkim429@gmail.com>
2019-04-23 16:55:54 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+
+  def roster
+    @roster
+  end
+
+  def grade(grade)
+    @roster.map do |level, students_array|
+      if level == grade
+        return students_array
+      end
+    end
+  end
+
+  def sort
+    sorted = {}
+    @roster.each do |level, students_array|
+      sorted[level] = students_array.sort
+    end
+    sorted
+  end
+
+end
