c37e743d600fdaccd371f825aa49cbf807e1c6c3
Caleb Watters <caleb@h2os.org>
2019-04-23 15:18:55 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+    attr_accessor :name, :grade, :roster
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+
+        if @roster[grade] == nil
+            @roster[grade] = [name]
+        else
+            @roster[grade] << name
+        end
+    
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_hash = {}
+        
+        @roster.each { |key, value|
+            sorted_hash[key] = value.sort
+        }
+        return sorted_hash
+    end
+end
