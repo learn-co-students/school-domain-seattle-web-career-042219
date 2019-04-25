47141c2275faf3b0a03ae7169c50b74409e653cc
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-04-25 16:03:04 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+	attr_reader :name, :roster
+
+
+
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def add_student(name, grade)
+		if @roster[grade] != nil
+		  @roster[grade] << name
+		else
+		  @roster[grade] = [name]
+		end
+	end
+
+	def grade(grade_num)
+		@roster[grade_num]
+	end
+
+	def sort
+		sorted_hash = {}
+
+		@roster.each do |grades, names|
+			sorted_hash[grades] = names.sort
+		end
+		sorted_hash
+	end
+
+end
+
+school = School.new("Bayside High School")
